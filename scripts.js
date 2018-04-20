0ebb25051a2ad7b2d805c223a83070c7b31f452d
Bruce Hong <32785837+brucejh99@users.noreply.github.com>
2018-04-20 10:34:11 -0400

    With basic moving background

--- scripts.js
+++ scripts.js
@@ -113,3 +113,18 @@ $(document).ready(function(){
         $(this).removeClass("active-orange");
     });
 });
+
+
+$(document).ready(function(){
+    var $win = $(window);
+
+    $('div.background').each(function(){
+        var scroll_speed = 10;
+        var $this = $(this);
+        $(window).scroll(function() {
+            var bgScroll = -(($win.scrollTop() - $this.offset().top)/scroll_speed);
+            var bgPosition = 'center '+ bgScroll + 'px';
+            $this.css({ backgroundPosition: bgPosition });
+        });
+    });
+});
