f04b7f98d919dfe8772abaeb2c78e55561974dd9
brucejh99 <bjhong@yahoo.com>
2020-04-04 20:48:50 -0400

    Updates

---
+++ precache-manifest.80eeb9f6d0e915301b8e0b0d9fa31e7b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "358882ddaf380de46e4acfe6d8380555",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bda69689dbcaadbf259e",
+    "url": "/static/css/main.ca327971.chunk.css"
+  },
+  {
+    "revision": "0b26cace0872defe10fd",
+    "url": "/static/js/2.8136407d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8136407d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bda69689dbcaadbf259e",
+    "url": "/static/js/main.bedc45f4.chunk.js"
+  },
+  {
+    "revision": "c4d28bc7a8dd139ad97b",
+    "url": "/static/js/runtime-main.83f8b661.js"
+  },
+  {
+    "revision": "972d54ac7985dbc9a8014a49a29c7668",
+    "url": "/static/media/ShareTechMono.972d54ac.ttf"
+  }
+]);
